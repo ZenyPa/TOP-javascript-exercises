ccb44111d1602dec8515c831c52fe575db34fb40
ZenyPa <paulzenevicius@gmail.com>
2025-10-27 23:11:15 +0200

    Solved 06 Repeat String

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
 
-};
+
+
+const repeatString = function(string, num) {
+    if (typeof string !== "string" || num < 0 || !Number.isInteger(num)) {
+        return 'ERROR'; 
+    }
+    else { 
+        let output = '';
+    
+        for (let i = 0; i < num; i++) {
+            output += string;
+        }
+        return output;
+    }
+
+    }
+ 
 
 // Do not edit below this line
 module.exports = repeatString;
